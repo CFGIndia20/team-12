da4a3d79b1ec132f990ee7ef0c9964e822d061dd
dhawalmali <dhawal.mali@yahoo.com>
2020-07-25 15:44:19 +0530

    twilio sms and whatsapp integration

---
+++ server/app.js
@@ -0,0 +1,43 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const request = require('request');
+const app = express();
+const dialogflowSessionClient =
+    require('./botlib/dialogflow_session_client.js');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const projectId = process.env.DIALOGFLOW_PROJECT_ID;
+const phoneNumber = process.env.TWILIO_PHONE_NUM;
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+
+const client = require('twilio')(accountSid, authToken);
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
+const sessionClient = new dialogflowSessionClient(projectId);
+
+const listener = app.listen(3000, function() {
+  console.log('Your Twilio integration server is listening on port '
+      + listener.address().port);
+});
+
+app.post('/', async function(req, res) {
+  const body = req.body;
+  const text = body.Body;
+  const id = body.From;
+  const dialogflowResponse = (await sessionClient.detectIntent(
+      text, id, body)).fulfillmentText;
+  const twiml = new  MessagingResponse();
+  const message = twiml.message(dialogflowResponse);
+  res.send(twiml.toString());
+});
+
+process.on('SIGTERM', () => {
+  listener.close(() => {
+    console.log('Closing http server.');
+    process.exit(0);
+  });
+});
